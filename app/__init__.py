c58230ef022b4e529ff451d98f8d7d3702150a2a
ajaykumar <kanamuajaykumar@gmail.com>
2026-04-29 22:39:09 +0530

    Fixed additional backend regression issues

--- app/__init__.py
+++ app/__init__.py
@@ -241,15 +241,16 @@ def create_app(config_name=None):
     CORS(
         app,
         resources={r"/*": {"origins": allowed_origins}},
+    )
     allowed_origins = [
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
         "https://staging.sfcollab.com",
         "https://sfcollab.com",
-        "https://sfclb.netlify.app"
+        "https://sfclb.netlify.app",
     ]
 
-    print(f"🚀 CORS ACTIVE FOR: {allowed_origins}")
+    print(f"🚀 CORS ACTIVE FOR: {allowed_origins}"),
 
     CORS(app, resources={r"/*": {"origins": allowed_origins}}, 
 
